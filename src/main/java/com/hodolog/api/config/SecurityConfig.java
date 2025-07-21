65fe9fcee894a5458be8c95ec4a8745960dc3bd6
chanhyoung <chlee3927@gmail.com>
2025-07-21 14:19:06 +0900

    SecurityConfig 개선

--- src/main/java/com/hodolog/api/config/SecurityConfig.java
+++ src/main/java/com/hodolog/api/config/SecurityConfig.java
@@ -4,17 +4,17 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -33,52 +33,47 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Configuration
 @EnableWebSecurity(debug = false)
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+public class SecurityConfig {
+
 	private final Http401Handler http401Handler;
 	private final Http403Handler http403Handler;
 	private final TokenProvider tokenProvider;
-	
-	@Override
-	public void configure(WebSecurity web) {
-		web
-			.ignoring()
-			.antMatchers(
-//					"/courses/**",
-					"/h2-console/**",
-					"favicon.ico",
-					"/js/**",
-					"/css/**",
-					"/fonts/**",
-					"/img/**"
-			);
-			
+
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return (web) -> web.ignoring()
+				.antMatchers(
+						// "/courses/**",
+						"/h2-console/**",
+						"favicon.ico",
+						"/js/**",
+						"/css/**",
+						"/fonts/**",
+						"/img/**");
 	}
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .cors(withDefaults())
-                .exceptionHandling(handling -> handling
-                        .authenticationEntryPoint(http401Handler)
-                        .accessDeniedHandler(http403Handler))
-                .sessionManagement(management -> management
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeRequests(requests -> requests
-                        .antMatchers("/auth/login").permitAll()
-                        .antMatchers("/auth/signup").permitAll()
-                        .antMatchers("/courses").permitAll()
-//				.antMatchers("/courses/**").permitAll()
-                        .antMatchers("/api/user").hasRole("USER")
-                        .antMatchers("/api/admin").hasRole("ADMIN")
-                        .anyRequest().authenticated())
-                .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class)
-			;
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+				.csrf(csrf -> csrf.disable())
+				.cors(withDefaults())
+				.exceptionHandling(handling -> handling
+						.authenticationEntryPoint(http401Handler)
+						.accessDeniedHandler(http403Handler))
+				.sessionManagement(management -> management
+						.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(requests -> requests
+						.antMatchers("/auth/login", "/auth/signup", "/courses").permitAll()
+						// .antMatchers("/courses/**").permitAll()
+						.antMatchers("/api/user").hasRole("USER")
+						.antMatchers("/api/admin").hasRole("ADMIN")
+						.anyRequest().authenticated())
+				.addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
+		return http.build();
 	}
-	
+
 	@Bean
 	UserDetailsService userDetailsService(UserRepository userRepository) {
 		return new UserDetailsService() {
@@ -86,28 +81,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 				User user = userRepository.findByEmail(username)
 						.orElseThrow(() -> new UsernameNotFoundException(username + "을 찾을 수 없습니다."));
-				
+
 				log.info(">>>user={}", user);
 				return new UserPrincipal(user);
 			}
 		};
 	}
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
+	@Bean
+	PasswordEncoder passwordEncoder() {
 		return new SCryptPasswordEncoder();
 	}
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		
-//		configuration.addAllowedOrigin("*");
+
+		// configuration.addAllowedOrigin("*");
 		configuration.addAllowedOriginPattern("*");
 		configuration.addAllowedHeader("*");
 		configuration.addAllowedMethod("*");
 		configuration.setAllowCredentials(true);
-		
+
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
